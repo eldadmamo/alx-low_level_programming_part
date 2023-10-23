e3c096c36bf943723beddfbaf2886ce7aa3652a0
Eldad <eldadf456@gmail.com>
2023-10-23 13:57:12 -0700

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,17 +1,12 @@
 #include "main.h"
 
 /**
- *   * set_string - main function
- *     *
- *       * @s: Function parameter
- *         *
- *           * @to: Function parameter
- *             *
- *               * Return: Always 0.
+ *  * set_string - sets the value of a pointer to a char
+ *   * @s: pointer to pointer
+ *    * @to: pointer char
 */
-void set_string(char **s, char *to)
 
+void set_string(char **s, char *to)
 {
 		*s = to;
-
 }
