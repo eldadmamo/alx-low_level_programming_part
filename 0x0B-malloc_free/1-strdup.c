e0720a5985ae711c4d749c8b2be8ffdacbf71fe1
Eldad <eldadf456@gmail.com>
2023-10-30 16:02:22 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,16 +13,13 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-
 	i = 0;
 	while (str[i] != '\0')
 		i++;
-
-	aaa = malloc(sizeof(char) * (i + 1);
-			if (aaa == NULL)
-			return (NULL);
-
-			for (r = 0; str[r]; r++)
-			aaa[r] = str[r];
-			return (aaa);
+	aaa = malloc(sizeof(char) * (i + 1));
+	if (aaa == NULL)
+		return (NULL);
+	for (r = 0; str[r]; r++)
+		aaa[r] = str[r];
+	return (aaa);
 }
