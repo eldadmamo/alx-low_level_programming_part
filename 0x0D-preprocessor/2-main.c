04477b849c2c73d8f1543a69b56091031223a6fa
Eldad <eldadf456@gmail.com>
2023-11-06 11:50:56 -0800

    0x0D. C - Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+* main - The main function is the entry point of the program.
+*
+* Return: 0 (if the program executes successfully)
+*/
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
