a1785edc362c415489af9ef9ba510a5f27529d77
Eldad <eldadf456@gmail.com>
2023-11-27 12:19:04 -0800

    0x15-file_io

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -7,6 +7,17 @@
 #include <unistd.h>
 #include "main.h"
 
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
 /**
  * check_elf - Verifies the presence of an ELF file.
  * @e_ident: Pointer array containing ELF magic numbers.
